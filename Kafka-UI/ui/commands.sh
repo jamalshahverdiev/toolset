809ba948b68d4583eb57ab1028a4be6766321c2f
Jamal Shahverdiyev <jamal.shahverdiev@dif.tech>
2023-12-31 10:08:08 +0400

    Delete Kafka UI

--- Kafka-UI/ui/commands.sh
+++
@@ -1,3 +0,0 @@
-helm repo add kafka-ui https://provectus.github.io/kafka-ui-charts
-helm install kafka-ui kafka-ui/kafka-ui --namespace kafka --create-namespace -f values.yaml
-# helm install kafka-ui kafka-ui/kafka-ui --namespace kafka --create-namespace --set yamlApplicationConfigConfigMap.name="kafka-ui-configmap",yamlApplicationConfigConfigMap.keyName="config.yml"
